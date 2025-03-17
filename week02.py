eb182ea703bc3c315f4330edba0f2af17d2305df
zzingni <wldms001313@gamil.com>
2025-03-17 11:43:34 +0900

    v.0.2 0(1) update

--- week02.py
+++ week02.py
@@ -4,6 +4,5 @@
 n = int(input("10 단위 수 입력: "))
 
 result = 0
-for i in range(1, n+1):
-    result += i
+result = n * (n+1) // 2 # O(1) 루프 반복 안 함, 속도 향상 >> 상수 시간
 print(result) # 실행 alt + shift + F10
