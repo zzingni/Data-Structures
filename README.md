02d38346a28111eae7498a6c97c49423b70a0fb0
zzingni <wldms001313@gamil.com>
2025-03-17 11:32:16 +0900

    v0.1 B &(n) & README.md & .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+2025 Data-Structures Class
+
+This remote repository uploads the codes used in the 2025 data structure class.
---
+++ week02.py
@@ -0,0 +1,9 @@
+
+
+
+n = int(input("10 단위 수 입력: "))
+
+result = 0
+for i in range(1, n+1):
+    result += i
+print(result) # 실행 alt + shift + F10
