1a7c3a52366a73bab32d1d6c5249cdc04bd60d9b
zzingni <wldms001313@gamil.com>
2025-03-24 12:11:11 +0900

    1.0 ㅑãintersection

--- week03.py
+++ week03.py
@@ -1,12 +1,10 @@
 
-def interection(l1, l2):
-    l3 = list()
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+def inters(l1, l2):
+    s1 = set(l1)
+    s2 = set(l2)
+    return list(s1.intersection(s2)) # 교집합
 
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
-print(interection(l1, l2))
+print(inters(l1, l2))
