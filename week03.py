ad0ebd5f21c4bc932db37c43d5ad896535d777e7
zzingni <wldms001313@gamil.com>
2025-03-24 11:38:57 +0900

    v0.6 zip array

--- week03.py
+++ week03.py
@@ -1,21 +1,7 @@
 
 
-def move_zeros(a_list):
-    zero_index = 0
-    for index, n in enumerate(a_list):
-        if n != 0:
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-            zero_index += 1
-    return(a_list)
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+ratings = [1, 2, 4, 3] # 정상적으로 동작하려면 리스트의 원소 개수가 동일해야 함. (실행은 되지만 없는 원소만큼 짤림)
 
-
-a_list  = [8, 0, 3, 0, 12]
-move_zeros(a_list)
-print(a_list)
-
-
-l = [11, 0, 9, 0, -77]
-move_zeros(l)
-print(l)
+groups_rating = list(zip(groups, ratings))
+print(groups_rating)
