014de6759dfdae86591108e1e3fcaddd7d5a7b2a
zzingni <wldms001313@gamil.com>
2025-03-24 12:14:25 +0900

    1.01 intersection update

--- week03.py
+++ week03.py
@@ -3,7 +3,7 @@ def inters(l1, l2):
     s1 = set(l1)
     s2 = set(l2)
     return list(s1.intersection(s2)) # 교집합
-
+    # return list(s1 & s2)
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
