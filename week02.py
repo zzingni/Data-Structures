e9b6280ab23eae07029fb335a24ee37dfbe722b9
zzingni <wldms001313@gamil.com>
2025-03-24 10:45:39 +0900

    v0.3 array

--- week02.py
+++ week02.py
@@ -6,3 +6,6 @@ n = int(input("10 단위 수 입력: "))
 result = 0
 result = n * (n+1) // 2 # O(1) 루프 반복 안 함, 속도 향상 >> 상수 시간
 print(result) # 실행 alt + shift + F10
+
+
+# 로그시간을 갖는 숫자 맞추기 게임 > 깃허브 올리기 (고 > 파이썬)
---
+++ week03.py
@@ -0,0 +1,8 @@
+
+
+array = [11, 9, -77, 8]
+
+for i in range(len(array)):
+    print(f"{array[i]:3}, {id(array[i])}")
+
+
