1cf9bebe9c670434627f2a402e32ec4f7f79294c
zzingni <wldms001313@gamil.com>
2025-03-24 11:45:28 +0900

    v0.7 set

--- week03.py
+++ week03.py
@@ -1,7 +1,7 @@
 
 
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-ratings = [1, 2, 4, 3] # 정상적으로 동작하려면 리스트의 원소 개수가 동일해야 함. (실행은 되지만 없는 원소만큼 짤림)
+city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+# city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
 
-groups_rating = list(zip(groups, ratings))
-print(groups_rating)
+city = set(city) # set 함수 인수로 리스트나 튜플 주면 중복값 제거
+print(city)
