b690825fee8cd7153216185bf4497deed1af2dfe
zzingni <wldms001313@gamil.com>
2025-03-24 11:30:08 +0900

    v0.5 move zero

--- week03.py
+++ week03.py
@@ -1,9 +1,21 @@
 
-import array
 
-arr = array.array('f',[11, 9, -77, 8]) # 하나의 문자열로 작동
+def move_zeros(a_list):
+    zero_index = 0
+    for index, n in enumerate(a_list):
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
+    return(a_list)
 
-for i in range(len(arr)):
-    print(f"{arr[i]:3}, {id(arr[i])}")
-print(arr[2])
 
+a_list  = [8, 0, 3, 0, 12]
+move_zeros(a_list)
+print(a_list)
+
+
+l = [11, 0, 9, 0, -77]
+move_zeros(l)
+print(l)
