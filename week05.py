fee828c9680250ab78cf09b7d7a5133f88e34432
zzingni <wldms001313@gamil.com>
2025-04-07 11:44:22 +0900

    v0.4 python list stack

--- week05.py
+++ week05.py
@@ -1,43 +1,16 @@
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty. Nothing to delete~"
-        popped_node = self.top
-        self.top = self.top.link
-        return popped_node.data # 데이터 삭제가 자동으로 된 건가...?? 생각해보기...  뭐지 ㅇㅅㅇ
-    # 티스토리에 포인터 활용한 c코드로 공부해서 올려보기 / 파이썬은 포인터가 없어서 참조형식으로 코드 짬
-
-
-s1 = Stack()
+s1 = list()
+s1.append("Data structure") # push
+s1.append("Database") # push
+print(len(s1))
+print(s1)
 print(s1.pop())
-
-# 1번 push 후 pop 2번
-#s1.push("자료구조")
-#print(s1.pop())
-#print(s1.pop())
-
-s1.push("자료구조")
-s1.push("데이터베이스")
-# print(s1.pop()) # 데이터베이스부터 pop됨
+print(s1)
+print(s1[-1]) # 마지막 요소 확인 = peek
+print(s1.pop())
+print(s1)
 # print(s1.pop())
+# print(s1)
 
-for i in range(2): # index 벗어나면 스택이 비었기 때문에 에러 발생
-    print(s1.pop())
+# 파이썬의 list 사용하면 class 없이 스택처럼 사용 가능
 
