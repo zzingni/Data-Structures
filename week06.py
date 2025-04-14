f3c77c6ce6a12f1b5c128097c6756fbdcb36266b
zzingni <wldms001313@gamil.com>
2025-04-14 11:35:09 +0900

    v0.1 queue

---
+++ week06.py
@@ -0,0 +1,27 @@
+
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+
+    def enqueue(self, data):
+        self.size = self.size + 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.link = node
+            self.rear = node
+
+q = Queue()
+q.enqueue("자료구조")
+q.enqueue("데이터베이스")
+print(q.size, q.front.data, q.rear.data)
