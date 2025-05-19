2878bcac5dc03f4f66ae2ad35d01e9caa1ba19f4
zzingni <wldms001313@gamil.com>
2025-05-19 10:52:01 +0900

    v0.1

--- week11.py
+++ week11.py
@@ -6,10 +6,6 @@ class Graph:
 
 G1 = Graph(4)
 
-G3 = Graph(4)
-G_self = Graph(4)
-
-
 # 0 == A, 1 == B, 2 == C, 3 == D
 G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
 G1.graph[1][0] = 1; G1.graph[1][2] = 1
@@ -23,19 +19,22 @@ for row in range(G1.SIZE):
     print()
 
 
+G2 = Graph(4)
+
 # 0 == A, 1 == B, 2 == C, 3 == D
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
 
 print()
 
-print("# G3 방향 그래프 #")
-for row in range(G3.SIZE):
-    for col in range(G3.SIZE):
-        print(G3.graph[row][col], end=' ')
+print("# G2 방향 그래프 #")
+for row in range(G2.SIZE):
+    for col in range(G2.SIZE):
+        print(G2.graph[row][col], end=' ')
     print()
 
 
+G_self = Graph(4)
 # 0 == A, 1 == B, 2 == C, 3 == D
 G_self.graph[0][3] = 1
 G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
@@ -44,7 +43,7 @@ G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
 
 print()
 
-print("# G_slef 무방향 그래프 #")
+print("# G_self 무방향 그래프 #")
 for row in range(G_self.SIZE):
     for col in range(G_self.SIZE):
         print(G_self.graph[row][col], end=' ')
